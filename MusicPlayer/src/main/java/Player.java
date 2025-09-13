3206eb0613dd77ef67317165ab6c2bb9a4967a1c
Gustavo Silva <gustavomachado.gms@hotmail.com>
2025-09-13 17:32:01 -0300

    first commit

---
+++ MusicPlayer/src/main/java/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+
+    private int nota;
+    private int instrumento;
+    private int volume;
+    private int oitava;
+
+    public void Play() {
+
+    }
+}
---
+++ MusicPlayer/src/main/java/TextHandler.java
@@ -0,0 +1,32 @@
+public class TextHandler {
+
+    private String text;
+    private int currentChar;
+
+    public TextHandler(String text) {
+        this.text = text;
+        this.currentChar = 0;
+    }
+
+    public char getNextChar() {
+        char nextChar = '0';
+
+        if (hasNextChar()) {
+            nextChar = text.charAt(currentChar);
+            currentChar++;
+        }
+
+        return nextChar;
+    }
+
+    public boolean hasNextChar() {
+
+        boolean hasNextChar = false;
+
+        if ((currentChar <= (text.length() - 1)) && (currentChar >= 0)) {
+            hasNextChar = true;
+        }
+
+        return hasNextChar;
+    }
+}
