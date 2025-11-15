1278a85f08372225b8f963d6a21c2a155b38c599
Gustavo Silva <gustavomachado.gms@hotmail.com>
2025-11-15 14:12:23 -0300

    textHandler modified for accept BPM+ string

--- MusicPlayer/src/main/java/com/tcp/musicPlayer/model/inputHandlers/Constants.java
+++ MusicPlayer/src/main/java/com/tcp/musicPlayer/model/inputHandlers/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final int INPUT_TEXT = 0;
     public static final int FILE_INPUT = 1;
-    public static final int TEXT_END = 'f';
+    public static final String TEXT_END = 'f';
 
 
 }
